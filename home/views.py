8224c459528f1336196b997f0b5e55041a3102e2
Aryan <aryanjainak@gmail.com>
2022-09-07 15:57:53 +0530

    changes

--- home/views.py
+++ home/views.py
@@ -14,20 +14,22 @@ def order(request):
     if request.method == 'POST':
         checkin_dtime = request.POST.get('checkin_dtime')
         checkout_dtime= request.POST.get('checkout_dtime')
-        rooms_already_booked =Booking.objects.filter(checkin_dtime__gte=checkin_dtime,checkout_dtime__lte=checkout_dtime,booked=True).count()
-        #print(rooms_already_booked)
-        #print(f"{hotel.total_rooms=}\n{Booking.objects.filter(checkin_dtime__gte=checkin_dtime,checkout_dtime__lte=checkout_dtime,booked=False).count()=}")
+        rooms_already_booked =Booking.objects.all().count()
         number_of_rooms_left = hotel.total_rooms - rooms_already_booked
         if number_of_rooms_left  >= 1:
             entry=Booking(checkin_dtime=checkin_dtime,checkout_dtime=checkout_dtime)
             entry.save()
             return HttpResponseRedirect(reverse('index'))
         else:
-            messages.success(request,'Rooms full,Better luck next time!')
-            return render(request,'home/index.html',{'hotel':hotel})
+            finished_bookings_count = Booking.objects.filter(checkout_dtime__lte=checkin_dtime, booked=True).count()
+            if finished_bookings_count > 0:
+                entry=Booking(checkin_dtime=checkin_dtime,checkout_dtime=checkout_dtime)
+                entry.save()
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                messages.success(request,'Rooms full,Better luck next time!')
+                return render(request,'home/index.html',{'hotel':hotel})
     return render(request,'home/booking.html',{'hotel':hotel})
-
-
 
 """
 def order(request):
